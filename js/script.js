9120190d9e18bffcea81405e54063b7a1cff2438
IlbeyMahmutShn <m.sahin.51@hotmail.com>
2024-05-31 13:18:50 +0300

    Veritabanı bağlantısı yapıldı

--- js/script.js
+++ js/script.js
@@ -43,4 +43,27 @@ menuBtn.addEventListener("click", function () {
   });
 });
 
+const searchBox = document.getElementById('search-box');
+
+searchBox.addEventListener('keypress', function(e) {
+  // Enter tuşuna basıldığında
+  if (e.key === 'Enter') {
+    // Arama kutusundaki değeri al
+    const searchTerm = e.target.value.toLowerCase();
+    // Tüm sekmeleri seç
+    const sections = document.querySelectorAll('section');
+    // Her bir sekmeyi kontrol et
+    sections.forEach(function(section) {
+      // Her sekmeyi tek tek gez ve içeriğinde arama yap
+      const sectionContent = section.textContent.toLowerCase();
+      if (sectionContent.includes(searchTerm)) {
+        // Eğer aranan kelime içeriyorsa, o sekmeye git
+        window.scrollTo({ top: section.offsetTop, behavior: 'smooth' });
+        // Arama kutusunu temizle
+        e.target.value = '';
+      }
+    });
+  }
+});
+
 /* butonlar bitiş */
